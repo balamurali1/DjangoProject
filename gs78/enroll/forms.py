66bbe11cb496f3bd7801c4f28f7cc972fe11e570
Balamurali <balamurali1@gmail.com>
2021-08-30 19:05:48 +0530

    First Commit

---
+++ gs78/enroll/forms.py
@@ -0,0 +1,30 @@
+from django.contrib.auth.models import User
+from django import forms
+from django.contrib.auth.forms import UserCreationForm,UserChangeForm
+
+class SignUpForm(UserCreationForm):
+		# note:wroser lo kanabade password label ni  change cheyali ante ela
+	password2 = forms.CharField(label = 'Confirm Password(again)',
+		widget = forms.PasswordInput)
+
+	class Meta:
+		model = User
+		fields = ['username','first_name','last_name','email']
+				# note:wroser lo kanabade email label ni  change cheyali ante ela
+		labels = {'email':'Email'}
+
+class EditUserProfileForm(UserChangeForm):
+	password = None
+	class Meta:
+		model = User
+		fields = ['username','first_name','last_name','email',
+		'date_joined','last_login']
+		labels = {'email':'Email'}
+
+
+class EditAdminProfileForm(UserChangeForm):
+	password = None
+	class Meta:
+		model = User
+		fields = '__all__'
+		labels = {'email':'Email'}
