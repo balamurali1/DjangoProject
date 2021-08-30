6e1021f1fbab925d8a047a77a1b3f9dc24dcea83
Balamurali <balamurali1@gmail.com>
2021-08-30 18:46:23 +0530

    First commit

---
+++ gs60/enroll/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+
+class StudentRegistration(forms.Form):
+	name = forms.CharField()
+	email = forms.EmailField()
+	def clean(self):
+		cleaned_data = super().clean()
+		valname = self.cleaned_data['name']
+		valemail = self.cleaned_data['email']
+		if len(valname) < 4:
+			raise forms.ValidationError('Name should be more than or equal 4 char')
+
+		if len(valemail) < 10:
+			raise forms.ValidationError('Email should be more than or equal 10 char')	
+	
