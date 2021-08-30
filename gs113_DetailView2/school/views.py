5b98c2b81a2258c67752ba564a62208ca6658614
Balamurali <balamurali1@gmail.com>
2021-08-30 20:50:16 +0530

    First Commit

---
+++ gs113_DetailView2/school/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from . models import Student
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
+
+# Create your views here.
+
+class StudentDetailView(DetailView):
+	model = Student
+	template_name = 'school/student.html' 
+	
+
+class StudentListView(ListView):
+	model = Student	
+
+
+	# stud = Student.objects.all()
+	# context = {'student':stud}
+	# return render(request,'school/student.html',context)	
