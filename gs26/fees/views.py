ee51024e112bb2a17cb69c04e36f995d47b03bbb
Balamurali <balamurali1@gmail.com>
2021-08-30 18:18:26 +0530

    First Commit

---
+++ gs26/fees/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+
+def fess_django(request):
+    return render(request,'fees/feesone.html')
+
+def fess_python(request):
+    return render(request,'fees/fesstwo.html')
