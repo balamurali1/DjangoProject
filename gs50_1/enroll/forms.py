1e4cb5e437bdd85367ab91904f79fcfd0f8522df
Balamurali <balamurali1@gmail.com>
2021-08-30 18:41:33 +0530

    First Commit

---
+++ gs50_1/enroll/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+
+
+class StudentRegistration(forms.Form):
+	name = forms.CharField()  #'name' is the visible
+	email = forms.EmailField() #'email' is the visible
+	mobile = forms.IntegerField() #'mobile' is the visible
+	key = forms.CharField(widget = forms.HiddenInput()) #'key' is the visible
