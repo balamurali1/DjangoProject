5e94629ee4d73c86cfefec6212fd7e43a325e023
Balamurali <balamurali1@gmail.com>
2021-08-30 19:17:04 +0530

    First Commit

---
+++ gs84/student/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render,HttpResponse
+
+# Create your views here.
+
+def setsession(request):
+	request.session['name'] = 'Sonam' 
+	return render(request,'student/setsession.html')
+
+
+def getsession(request):	
+	name = request.session['name']
+	return render(request,'student/getsession.html',{'name':name})
+		
+
+def delsession(request):
+	request.session.flush()  #flush() idi use chesthe total data remove auvthundi.
+	request.session.clear_expired()
+	return render(request,'student/delsession.html')
