a0e348b4380ce5190e3d7b288abfbbcb886368f8
Balamurali <balamurali1@gmail.com>
2021-08-30 20:42:43 +0530

    First Commit

---
+++ gs110_RedirectView/school/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.views.generic.base import TemplateView,RedirectView
+
+# Create your views here.
+
+class GeekyShowsRedirectView(RedirectView):
+	url = 'https://geekyshows.com'
+
+
+
+# class GeekRedirectView(RedirectView):
+# 	url = '/'
+
+class GeekRedirectView(RedirectView):
+	pattern_name = 'mindex'	
+	permanent = True
+	query_string = True
+
+
+	def get_redirect_url(self,*args,**kwargs):
+		print(kwargs)
+		#kwargs['pk'] = 12	
+		return super().get_redirect_url(*args,**kwargs)	
