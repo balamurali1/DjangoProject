42c0a00b5951f153ea9dd5347c03369d3f0f1f87
Balamurali <balamurali1@gmail.com>
2021-08-30 21:11:17 +0530

    First Commit

---
+++ schoolproject/course/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def index(request):
+  return HttpResponse('Home Page')
+
+def learn_django(request):
+  return HttpResponse('Hello Django')
+
+
+def learn_python(request):
+  return HttpResponse('<h1>Hello Django</h1>')
+
+def learn_var(request):
+  a = '<h1>Hello variable</h1>'
+  return HttpResponse(a)
+
+
+def learn_math(request):
+  a = 10 + 10
+  return HttpResponse(a)
+
+def learn_format(request):
+  a = 'GeekyShows'
+  return HttpResponse(f'Hello How are You {a}')
+  
